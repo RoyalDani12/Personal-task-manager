3512d2ce93f04d97f48716b359b529d233cdcf23
Daniel Abraraw <danielabraraw20@gmail.com>
2026-03-21 21:03:30 +0300

    Stope task API complete

---
+++ src/api/stop.task.api.jsx
@@ -0,0 +1,11 @@
+import api from "./api.Centeral";
+
+export const stopTaskApi = async(taskId)=>{
+   try {
+   const response = await api.post(`/tasks/stop-task/${taskId}`)
+   return response
+   } catch (error) {
+    console.log("Stop api error ",error.message)
+    throw error
+   }
+}
